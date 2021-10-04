94d9ec8cc1e28d225ad071ec21be9364ed296546
julioderose11 <julioderose11@gmail.com>
2021-10-04 17:21:21 +0200

    Added Customer class

---
+++ Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Business/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phumla_Kamnandi_Hotel.Business
+{
+    class Customer
+    {
+        private string name;
+    }
+}
