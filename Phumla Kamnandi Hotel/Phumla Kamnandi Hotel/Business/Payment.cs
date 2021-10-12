23fbee69dc68a43dad5bacefae55f29f1c0babb6
Max <maxterrycoleman@gmail.com>
2021-10-12 10:31:26 +0200

    Payment Class

---
+++ Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Business/Payment.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phumla_Kamnandi_Hotel.Business
+{
+    class Payment
+    {
+        #region Fields
+        private string paymentID, paymentType;
+        private DateTime paymentDate;
+        private decimal amount;
+        //payment array needs to be added
+        #endregion
+
+        #region Property Methods
+        public string getPaymentID
+        {
+            get { return paymentID; }
+            set { paymentID = value; }
+        }
+
+        public string getPaymentType
+        {
+            get { return paymentType; }
+            set { paymentType = value; }
+        }
+
+        public DateTime getPaymentDate
+        {
+            get { return paymentDate; }
+            set { paymentDate = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public Payment()
+        {
+            paymentID = "";
+            paymentType = "";
+            paymentDate = default(DateTime);
+            amount = 0;
+        }
+
+        public Payment(string pID, string pType, DateTime pDate, decimal amnt)
+        {
+            paymentID = pID;
+            paymentType = pType;
+            paymentDate = pDate;
+            amount = amnt;
+        }
+        #endregion
+    }
+}
