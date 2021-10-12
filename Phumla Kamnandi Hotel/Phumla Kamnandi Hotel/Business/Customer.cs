54121be326614b123b16493d5c3159de71af4869
julioderose <julioderose11@gmail.com>
2021-10-12 09:47:11 +0200

    customer changes

--- Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Business/Customer.cs
+++ Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Business/Customer.cs
@@ -6,12 +6,34 @@ using System.Threading.Tasks;
 
 namespace Phumla_Kamnandi_Hotel.Business
 {
-    class Customer
+    class Customer : Person
     {
         #region Fields
         private string customerID;
         private int mobilePhone;
         private int noOfGuests;
         #endregion
+
+        public string CustomerID
+        {
+            get { return customerID; }
+            set { customerID = value; }
+        }
+
+        public Customer()
+        {
+            customerID = "";
+            mobilePhone = 0;
+            noOfGuests = 0;
+        }
+
+        public Customer(string cID, int cPhone, int cGuests)
+        {
+            customerID = cID;
+            mobilePhone = cPhone;
+            noOfGuests = cGuests;
+        }
+
+
     }
 }
