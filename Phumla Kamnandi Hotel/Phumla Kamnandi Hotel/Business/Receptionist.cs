218295cf72fafa1f128f2c47b96e8d5ddc9101f7
James-Clarkk <clrjam006@myuct.ac.za>
2021-10-12 10:02:05 +0200

    Add changes

---
+++ Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Business/Receptionist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phumla_Kamnandi_Hotel.Business
+{
+    class Receptionist
+    {
+        #region: constructor
+        public Receptionist(): base()
+        {
+
+        }
+        #endregion
+
+        #region: methods
+
+        #endregion
+    }
+}
