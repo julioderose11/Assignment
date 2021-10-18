eb24b545ffa2cd314339760272a5c54506509ed1
julioderose <julioderose11@gmail.com>
2021-10-18 18:51:05 +0200

    MDI startup

--- Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Program.cs
+++ Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Program.cs
@@ -16,7 +16,7 @@ namespace Phumla_Kamnandi_Hotel
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new BookingMDIParent());
         }
     }
 }
