7ce4b999957a3d0fe4193f9ce909e09213c39799
JackScottKing <jackscottking3@gmail.com>
2021-10-24 17:00:55 +0200

    Changes

--- Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Presentation/OccupancyReport.Designer.cs
+++ Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Presentation/OccupancyReport.Designer.cs
@@ -30,16 +30,16 @@ namespace Phumla_Kamnandi_Hotel.Presentation
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtAccount = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.lblTotalBooking = new System.Windows.Forms.Label();
             this.lblEndDate = new System.Windows.Forms.Label();
             this.lblStartDate = new System.Windows.Forms.Label();
-            this.txtStartDate = new System.Windows.Forms.TextBox();
-            this.lblTotalBooking = new System.Windows.Forms.Label();
-            this.txtTotal = new System.Windows.Forms.TextBox();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.bookingListView = new System.Windows.Forms.ListView();
             this.lblMessage = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.btnHome = new System.Windows.Forms.Button();
+            this.dTPStartDate = new System.Windows.Forms.DateTimePicker();
+            this.dTPEndDate = new System.Windows.Forms.DateTimePicker();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,15 +57,26 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Occupancy Details";
             // 
-            // txtAccount
+            // txtTotal
             // 
-            this.txtAccount.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtAccount.Location = new System.Drawing.Point(223, 130);
-            this.txtAccount.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtAccount.Name = "txtAccount";
-            this.txtAccount.ReadOnly = true;
-            this.txtAccount.Size = new System.Drawing.Size(185, 27);
-            this.txtAccount.TabIndex = 61;
+            this.txtTotal.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(179, 43);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(185, 27);
+            this.txtTotal.TabIndex = 63;
+            this.txtTotal.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblTotalBooking
+            // 
+            this.lblTotalBooking.AutoSize = true;
+            this.lblTotalBooking.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalBooking.Location = new System.Drawing.Point(6, 47);
+            this.lblTotalBooking.Name = "lblTotalBooking";
+            this.lblTotalBooking.Size = new System.Drawing.Size(128, 18);
+            this.lblTotalBooking.TabIndex = 62;
+            this.lblTotalBooking.Text = "Total Bookings:";
             // 
             // lblEndDate
             // 
@@ -87,45 +98,14 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             this.lblStartDate.TabIndex = 55;
             this.lblStartDate.Text = "Start Date:";
             // 
-            // txtStartDate
+            // bookingListView
             // 
-            this.txtStartDate.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtStartDate.Location = new System.Drawing.Point(223, 76);
-            this.txtStartDate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtStartDate.Name = "txtStartDate";
-            this.txtStartDate.ReadOnly = true;
-            this.txtStartDate.Size = new System.Drawing.Size(185, 27);
-            this.txtStartDate.TabIndex = 60;
-            // 
-            // lblTotalBooking
-            // 
-            this.lblTotalBooking.AutoSize = true;
-            this.lblTotalBooking.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotalBooking.Location = new System.Drawing.Point(6, 47);
-            this.lblTotalBooking.Name = "lblTotalBooking";
-            this.lblTotalBooking.Size = new System.Drawing.Size(128, 18);
-            this.lblTotalBooking.TabIndex = 62;
-            this.lblTotalBooking.Text = "Total Bookings:";
-            // 
-            // txtTotal
-            // 
-            this.txtTotal.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTotal.Location = new System.Drawing.Point(179, 43);
-            this.txtTotal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtTotal.Name = "txtTotal";
-            this.txtTotal.ReadOnly = true;
-            this.txtTotal.Size = new System.Drawing.Size(185, 27);
-            this.txtTotal.TabIndex = 63;
-            this.txtTotal.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // listView1
-            // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(51, 215);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(920, 191);
-            this.listView1.TabIndex = 75;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.bookingListView.HideSelection = false;
+            this.bookingListView.Location = new System.Drawing.Point(51, 215);
+            this.bookingListView.Name = "bookingListView";
+            this.bookingListView.Size = new System.Drawing.Size(920, 191);
+            this.bookingListView.TabIndex = 75;
+            this.bookingListView.UseCompatibleStateImageBehavior = false;
             // 
             // lblMessage
             // 
@@ -157,23 +137,40 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             this.btnHome.Text = "Home";
             this.btnHome.UseVisualStyleBackColor = true;
             // 
+            // dTPStartDate
+            // 
+            this.dTPStartDate.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dTPStartDate.Location = new System.Drawing.Point(203, 76);
+            this.dTPStartDate.Name = "dTPStartDate";
+            this.dTPStartDate.Size = new System.Drawing.Size(292, 25);
+            this.dTPStartDate.TabIndex = 79;
+            // 
+            // dTPEndDate
+            // 
+            this.dTPEndDate.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dTPEndDate.Location = new System.Drawing.Point(203, 129);
+            this.dTPEndDate.Name = "dTPEndDate";
+            this.dTPEndDate.Size = new System.Drawing.Size(292, 25);
+            this.dTPEndDate.TabIndex = 80;
+            // 
             // OccupancyReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(1063, 638);
+            this.Controls.Add(this.dTPEndDate);
+            this.Controls.Add(this.dTPStartDate);
             this.Controls.Add(this.btnHome);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblMessage);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.txtAccount);
+            this.Controls.Add(this.bookingListView);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lblEndDate);
-            this.Controls.Add(this.txtStartDate);
             this.Controls.Add(this.lblStartDate);
             this.Name = "OccupancyReport";
             this.Text = "OccupancyReport";
+            this.Load += new System.EventHandler(this.OccupancyReport_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -186,13 +183,13 @@ namespace Phumla_Kamnandi_Hotel.Presentation
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox txtTotal;
         private System.Windows.Forms.Label lblTotalBooking;
-        private System.Windows.Forms.TextBox txtAccount;
         private System.Windows.Forms.Label lblEndDate;
         private System.Windows.Forms.Label lblStartDate;
-        private System.Windows.Forms.TextBox txtStartDate;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView bookingListView;
         private System.Windows.Forms.Label lblMessage;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.DateTimePicker dTPStartDate;
+        private System.Windows.Forms.DateTimePicker dTPEndDate;
     }
 }
--- Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Presentation/OccupancyReport.cs
+++ Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Presentation/OccupancyReport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -7,22 +8,136 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Phumla_Kamnandi_Hotel.Business;
 
 namespace Phumla_Kamnandi_Hotel.Presentation
 {
     public partial class OccupancyReport : Form
     {
-       
+        #region Info
+        //This form is used to show the occupancy levels at the hotel. It does this by calling methods from the controller to view roomBooking objects
+        #endregion
 
+        #region Variables
+        //declaring varoables 
+        private Collection<RoomBooking> roomBookings;
+        private Collection<Booking> bookings;
+        private RoomBooking roomBook;
+        private RoomBooking roomBook;
+        private BookingController bookingController;
+        public bool listFormClosed;//= true;
+        #endregion
 
-        public OccupancyReport()
+        #region Constructor
+        public OccupancyReport(BookingController cusController)
         {
             InitializeComponent();
+            bookingController = cusController;
+           this.Load += OccupancyReport_Load;
+           this.Activated += OccupancyReport_Activated;
         }
+        #endregion
+
+        #region events
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             //ignore 
         }
+
+        private void OccupancyReport_Load(object sender, EventArgs e)
+        {
+            //this ensure that the listview actually populates details
+            bookingListView.View = View.Details;
+
+        }
+
+        private void OccupancyReport_Activated(object sender, EventArgs e)
+        {
+            //this activates the form to operate
+            bookingListView.View = View.Details;
+           // setUpBookingListView();
+          
+        }
+
+        private void OccupancyReport_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            listFormClosed = true;
+        }
+
+        #endregion
+
+        #region Utility Methods
+        //clearall method clears the textbox of all text
+        private void ClearAll()
+        {
+           
+            txtTotal.Text = "";
+            bookingListView
+            
+
+        }
+
+        private void EnableEntries(bool value)
+        {
+            //this method enables and or unenables data entry into textboxes
+           
+            txtTotal.Enabled = value;
+            
+        }
+        private void PopulateTextBoxes(RoomBooking roombooking)
+        {
+
+
+        }
+
+
+
+        public void setUpBookingListView()
+        {
+            ListViewItem bookingDetails;
+            roomBookings = null;
+            bookingListView.Clear();
+
+            bookingListView.Columns.Insert(0, "Reference Number", 150, HorizontalAlignment.Left);
+            //bookingListView.Columns.Insert(1, "CustomerID", 120, HorizontalAlignment.Left);
+            //bookingListView.Columns.Insert(2, "AccountNum", 120, HorizontalAlignment.Left);
+            bookingListView.Columns.Insert(1, "CustomerRequests", 240, HorizontalAlignment.Left);
+            //bookingListView.Columns.Insert(4, "BookingDate", 120, HorizontalAlignment.Left);
+            bookingListView.Columns.Insert(2, "ArrivalDate", 150, HorizontalAlignment.Left);
+            bookingListView.Columns.Insert(3, "DepartureDate", 150, HorizontalAlignment.Left);
+            bookingListView.Columns.Insert(4, "numPeople", 120, HorizontalAlignment.Left);
+            bookingListView.Columns.Insert(5, "Amount Due", 120, HorizontalAlignment.Left);
+            bookingListView.Columns.Insert(6, "Deposit Amount", 120, HorizontalAlignment.Left);
+
+            roomBookings = bookingController.AllRoomBookings;
+            ro = bookingController.AllBookings;
+            accounts = bookingController.AllAccounts;
+            foreach (Booking booking in bookings)
+            {
+                foreach (RoomBooking roomBooking in roomBookings)
+                {
+                    if (booking.getAccountNum == account.AccountNum)
+                    {
+                        bookingDetails = new ListViewItem();
+                        bookingDetails.Text = booking.getBookingID.ToString();
+                        //bookingDetails.SubItems.Add(booking.getCustomerID.ToString());
+                        //bookingDetails.SubItems.Add(booking.getAccountNum.ToString());
+                        bookingDetails.SubItems.Add(booking.getCustomerRequests.ToString());
+                        //bookingDetails.SubItems.Add(booking.getBookingDate.ToString());
+                        bookingDetails.SubItems.Add(booking.getArrival.ToShortDateString());
+                        bookingDetails.SubItems.Add(booking.getDeparture.ToShortDateString());
+                        bookingDetails.SubItems.Add(booking.getNumPeople.ToString());
+                        bookingDetails.SubItems.Add(account.AmountDue.ToString());
+                        bookingDetails.SubItems.Add(account.DepositAmount.ToString());
+                        bookingListView.Items.Add(bookingDetails);
+                    }
+                }
+            }
+            bookingListView.Refresh();
+            bookingListView.GridLines = true;
+
+        }
+        #endregion
     }
 }
