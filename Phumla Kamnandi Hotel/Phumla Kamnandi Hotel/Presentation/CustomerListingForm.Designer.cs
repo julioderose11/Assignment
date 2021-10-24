32a9fd3fdfaba779240ac099a91dd6cc3c9edc59
JackScottKing <jackscottking3@gmail.com>
2021-10-24 16:08:30 +0200

    Occupancy Report

--- Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Presentation/CustomerListingForm.Designer.cs
+++ Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Presentation/CustomerListingForm.Designer.cs
@@ -56,9 +56,10 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             // 
             this.listLabel.AutoSize = true;
             this.listLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listLabel.Location = new System.Drawing.Point(24, 8);
+            this.listLabel.Location = new System.Drawing.Point(32, 10);
+            this.listLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.listLabel.Name = "listLabel";
-            this.listLabel.Size = new System.Drawing.Size(150, 17);
+            this.listLabel.Size = new System.Drawing.Size(176, 20);
             this.listLabel.TabIndex = 2;
             this.listLabel.Text = "Customer Database";
             this.listLabel.Click += new System.EventHandler(this.listLabel_Click);
@@ -67,10 +68,10 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             // 
             this.HomeBtn.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.HomeBtn.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.HomeBtn.Location = new System.Drawing.Point(789, 575);
-            this.HomeBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.HomeBtn.Location = new System.Drawing.Point(1052, 708);
+            this.HomeBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.HomeBtn.Name = "HomeBtn";
-            this.HomeBtn.Size = new System.Drawing.Size(75, 34);
+            this.HomeBtn.Size = new System.Drawing.Size(100, 42);
             this.HomeBtn.TabIndex = 65;
             this.HomeBtn.Text = "Home";
             this.HomeBtn.UseVisualStyleBackColor = false;
@@ -79,65 +80,64 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             // suburbTextBox
             // 
             this.suburbTextBox.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.suburbTextBox.Location = new System.Drawing.Point(155, 161);
-            this.suburbTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.suburbTextBox.Location = new System.Drawing.Point(207, 198);
+            this.suburbTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.suburbTextBox.Name = "suburbTextBox";
-            this.suburbTextBox.Size = new System.Drawing.Size(262, 23);
+            this.suburbTextBox.Size = new System.Drawing.Size(348, 27);
             this.suburbTextBox.TabIndex = 62;
             // 
             // streetNameTextBox
             // 
             this.streetNameTextBox.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.streetNameTextBox.Location = new System.Drawing.Point(155, 124);
-            this.streetNameTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.streetNameTextBox.Location = new System.Drawing.Point(207, 153);
+            this.streetNameTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.streetNameTextBox.Name = "streetNameTextBox";
-            this.streetNameTextBox.Size = new System.Drawing.Size(262, 23);
+            this.streetNameTextBox.Size = new System.Drawing.Size(348, 27);
             this.streetNameTextBox.TabIndex = 61;
             // 
             // emailTextBox
             // 
             this.emailTextBox.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailTextBox.Location = new System.Drawing.Point(155, 80);
-            this.emailTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.emailTextBox.Location = new System.Drawing.Point(207, 98);
+            this.emailTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.emailTextBox.Name = "emailTextBox";
-            this.emailTextBox.Size = new System.Drawing.Size(262, 23);
+            this.emailTextBox.Size = new System.Drawing.Size(348, 27);
             this.emailTextBox.TabIndex = 60;
             // 
             // nameTextBox
             // 
             this.nameTextBox.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nameTextBox.Location = new System.Drawing.Point(155, 38);
-            this.nameTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.nameTextBox.Location = new System.Drawing.Point(207, 47);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(262, 23);
+            this.nameTextBox.Size = new System.Drawing.Size(348, 27);
             this.nameTextBox.TabIndex = 59;
             // 
             // cusIDTextBox
             // 
             this.cusIDTextBox.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cusIDTextBox.Location = new System.Drawing.Point(600, 37);
-            this.cusIDTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.cusIDTextBox.Location = new System.Drawing.Point(800, 46);
+            this.cusIDTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cusIDTextBox.Name = "cusIDTextBox";
-            this.cusIDTextBox.Size = new System.Drawing.Size(178, 23);
+            this.cusIDTextBox.Size = new System.Drawing.Size(236, 27);
             this.cusIDTextBox.TabIndex = 58;
             // 
             // idTextBox
             // 
             this.idTextBox.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.idTextBox.Location = new System.Drawing.Point(600, 78);
-            this.idTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.idTextBox.Location = new System.Drawing.Point(800, 96);
+            this.idTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.idTextBox.Name = "idTextBox";
-            this.idTextBox.Size = new System.Drawing.Size(178, 23);
+            this.idTextBox.Size = new System.Drawing.Size(236, 27);
             this.idTextBox.TabIndex = 57;
             // 
             // suburbNameLabel
             // 
             this.suburbNameLabel.AutoSize = true;
             this.suburbNameLabel.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.suburbNameLabel.Location = new System.Drawing.Point(19, 161);
-            this.suburbNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.suburbNameLabel.Location = new System.Drawing.Point(25, 198);
             this.suburbNameLabel.Name = "suburbNameLabel";
-            this.suburbNameLabel.Size = new System.Drawing.Size(48, 17);
+            this.suburbNameLabel.Size = new System.Drawing.Size(61, 22);
             this.suburbNameLabel.TabIndex = 56;
             this.suburbNameLabel.Text = "Suburb";
             // 
@@ -145,10 +145,9 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             // 
             this.streetNameLabel.AutoSize = true;
             this.streetNameLabel.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.streetNameLabel.Location = new System.Drawing.Point(19, 124);
-            this.streetNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.streetNameLabel.Location = new System.Drawing.Point(25, 153);
             this.streetNameLabel.Name = "streetNameLabel";
-            this.streetNameLabel.Size = new System.Drawing.Size(40, 17);
+            this.streetNameLabel.Size = new System.Drawing.Size(51, 22);
             this.streetNameLabel.TabIndex = 55;
             this.streetNameLabel.Text = "Street";
             // 
@@ -156,10 +155,9 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             // 
             this.emailLabel.AutoSize = true;
             this.emailLabel.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailLabel.Location = new System.Drawing.Point(19, 84);
-            this.emailLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.emailLabel.Location = new System.Drawing.Point(25, 103);
             this.emailLabel.Name = "emailLabel";
-            this.emailLabel.Size = new System.Drawing.Size(36, 17);
+            this.emailLabel.Size = new System.Drawing.Size(47, 22);
             this.emailLabel.TabIndex = 54;
             this.emailLabel.Text = "Email";
             // 
@@ -167,10 +165,9 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             // 
             this.cusIDLabel.AutoSize = true;
             this.cusIDLabel.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cusIDLabel.Location = new System.Drawing.Point(500, 41);
-            this.cusIDLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.cusIDLabel.Location = new System.Drawing.Point(667, 50);
             this.cusIDLabel.Name = "cusIDLabel";
-            this.cusIDLabel.Size = new System.Drawing.Size(74, 17);
+            this.cusIDLabel.Size = new System.Drawing.Size(95, 22);
             this.cusIDLabel.TabIndex = 53;
             this.cusIDLabel.Text = "Customer ID";
             // 
@@ -178,10 +175,9 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             // 
             this.nameLabel.AutoSize = true;
             this.nameLabel.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nameLabel.Location = new System.Drawing.Point(19, 42);
-            this.nameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.nameLabel.Location = new System.Drawing.Point(25, 52);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(37, 17);
+            this.nameLabel.Size = new System.Drawing.Size(48, 22);
             this.nameLabel.TabIndex = 52;
             this.nameLabel.Text = "Name";
             // 
@@ -189,39 +185,37 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             // 
             this.idLabel.AutoSize = true;
             this.idLabel.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.idLabel.Location = new System.Drawing.Point(500, 84);
-            this.idLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.idLabel.Location = new System.Drawing.Point(667, 103);
             this.idLabel.Name = "idLabel";
-            this.idLabel.Size = new System.Drawing.Size(19, 17);
+            this.idLabel.Size = new System.Drawing.Size(24, 22);
             this.idLabel.TabIndex = 51;
             this.idLabel.Text = "ID";
             // 
             // PostalCodeTextBox
             // 
             this.PostalCodeTextBox.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PostalCodeTextBox.Location = new System.Drawing.Point(155, 241);
-            this.PostalCodeTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.PostalCodeTextBox.Location = new System.Drawing.Point(207, 297);
+            this.PostalCodeTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.PostalCodeTextBox.Name = "PostalCodeTextBox";
-            this.PostalCodeTextBox.Size = new System.Drawing.Size(142, 23);
+            this.PostalCodeTextBox.Size = new System.Drawing.Size(188, 27);
             this.PostalCodeTextBox.TabIndex = 70;
             // 
             // cityTextBox
             // 
             this.cityTextBox.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cityTextBox.Location = new System.Drawing.Point(155, 204);
-            this.cityTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.cityTextBox.Location = new System.Drawing.Point(207, 251);
+            this.cityTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cityTextBox.Name = "cityTextBox";
-            this.cityTextBox.Size = new System.Drawing.Size(262, 23);
+            this.cityTextBox.Size = new System.Drawing.Size(348, 27);
             this.cityTextBox.TabIndex = 69;
             // 
             // postalCodelbl
             // 
             this.postalCodelbl.AutoSize = true;
             this.postalCodelbl.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.postalCodelbl.Location = new System.Drawing.Point(19, 247);
-            this.postalCodelbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.postalCodelbl.Location = new System.Drawing.Point(25, 304);
             this.postalCodelbl.Name = "postalCodelbl";
-            this.postalCodelbl.Size = new System.Drawing.Size(73, 17);
+            this.postalCodelbl.Size = new System.Drawing.Size(93, 22);
             this.postalCodelbl.TabIndex = 68;
             this.postalCodelbl.Text = "Postal Code";
             // 
@@ -229,19 +223,19 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             // 
             this.cityNamelbl.AutoSize = true;
             this.cityNamelbl.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cityNamelbl.Location = new System.Drawing.Point(19, 204);
-            this.cityNamelbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.cityNamelbl.Location = new System.Drawing.Point(25, 251);
             this.cityNamelbl.Name = "cityNamelbl";
-            this.cityNamelbl.Size = new System.Drawing.Size(30, 17);
+            this.cityNamelbl.Size = new System.Drawing.Size(37, 22);
             this.cityNamelbl.TabIndex = 67;
             this.cityNamelbl.Text = "City";
             // 
             // customersListView
             // 
             this.customersListView.HideSelection = false;
-            this.customersListView.Location = new System.Drawing.Point(24, 28);
+            this.customersListView.Location = new System.Drawing.Point(32, 34);
+            this.customersListView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.customersListView.Name = "customersListView";
-            this.customersListView.Size = new System.Drawing.Size(840, 224);
+            this.customersListView.Size = new System.Drawing.Size(1119, 275);
             this.customersListView.TabIndex = 3;
             this.customersListView.UseCompatibleStateImageBehavior = false;
             this.customersListView.SelectedIndexChanged += new System.EventHandler(this.bookingListView_SelectedIndexChanged);
@@ -264,26 +258,28 @@ namespace Phumla_Kamnandi_Hotel.Presentation
             this.groupBox1.Controls.Add(this.emailTextBox);
             this.groupBox1.Controls.Add(this.suburbTextBox);
             this.groupBox1.Controls.Add(this.streetNameTextBox);
-            this.groupBox1.Location = new System.Drawing.Point(27, 272);
+            this.groupBox1.Location = new System.Drawing.Point(36, 335);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(837, 285);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1116, 351);
             this.groupBox1.TabIndex = 71;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Customer Information";
             // 
             // CustomerListingForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(911, 618);
+            this.ClientSize = new System.Drawing.Size(1215, 761);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.HomeBtn);
             this.Controls.Add(this.customersListView);
             this.Controls.Add(this.listLabel);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "CustomerListingForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ListingForm";
+            this.Text = "CustomerListingForm";
             this.Load += new System.EventHandler(this.CustomerListingForm_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
---
+++ Phumla Kamnandi Hotel/Phumla Kamnandi Hotel/Presentation/OccupancyReport.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Phumla_Kamnandi_Hotel.Presentation
+{
+    partial class OccupancyReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtAccount = new System.Windows.Forms.TextBox();
+            this.lblEndDate = new System.Windows.Forms.Label();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.txtStartDate = new System.Windows.Forms.TextBox();
+            this.lblTotalBooking = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtTotal);
+            this.groupBox1.Controls.Add(this.lblTotalBooking);
+            this.groupBox1.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(51, 459);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(444, 101);
+            this.groupBox1.TabIndex = 74;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Occupancy Details";
+            // 
+            // txtAccount
+            // 
+            this.txtAccount.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAccount.Location = new System.Drawing.Point(223, 130);
+            this.txtAccount.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtAccount.Name = "txtAccount";
+            this.txtAccount.ReadOnly = true;
+            this.txtAccount.Size = new System.Drawing.Size(185, 27);
+            this.txtAccount.TabIndex = 61;
+            // 
+            // lblEndDate
+            // 
+            this.lblEndDate.AutoSize = true;
+            this.lblEndDate.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEndDate.Location = new System.Drawing.Point(58, 134);
+            this.lblEndDate.Name = "lblEndDate";
+            this.lblEndDate.Size = new System.Drawing.Size(80, 18);
+            this.lblEndDate.TabIndex = 54;
+            this.lblEndDate.Text = "End Date:";
+            // 
+            // lblStartDate
+            // 
+            this.lblStartDate.AutoSize = true;
+            this.lblStartDate.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStartDate.Location = new System.Drawing.Point(58, 81);
+            this.lblStartDate.Name = "lblStartDate";
+            this.lblStartDate.Size = new System.Drawing.Size(96, 18);
+            this.lblStartDate.TabIndex = 55;
+            this.lblStartDate.Text = "Start Date:";
+            // 
+            // txtStartDate
+            // 
+            this.txtStartDate.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStartDate.Location = new System.Drawing.Point(223, 76);
+            this.txtStartDate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtStartDate.Name = "txtStartDate";
+            this.txtStartDate.ReadOnly = true;
+            this.txtStartDate.Size = new System.Drawing.Size(185, 27);
+            this.txtStartDate.TabIndex = 60;
+            // 
+            // lblTotalBooking
+            // 
+            this.lblTotalBooking.AutoSize = true;
+            this.lblTotalBooking.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalBooking.Location = new System.Drawing.Point(6, 47);
+            this.lblTotalBooking.Name = "lblTotalBooking";
+            this.lblTotalBooking.Size = new System.Drawing.Size(128, 18);
+            this.lblTotalBooking.TabIndex = 62;
+            this.lblTotalBooking.Text = "Total Bookings:";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(179, 43);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(185, 27);
+            this.txtTotal.TabIndex = 63;
+            this.txtTotal.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(51, 215);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(920, 191);
+            this.listView1.TabIndex = 75;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(47, 182);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(153, 20);
+            this.lblMessage.TabIndex = 76;
+            this.lblMessage.Text = "Occupancy Report";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 20);
+            this.label1.TabIndex = 77;
+            this.label1.Text = "Please select the Dates to view Occupancy";
+            // 
+            // btnHome
+            // 
+            this.btnHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.Location = new System.Drawing.Point(844, 519);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(111, 41);
+            this.btnHome.TabIndex = 78;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // OccupancyReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1063, 638);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.txtAccount);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblEndDate);
+            this.Controls.Add(this.txtStartDate);
+            this.Controls.Add(this.lblStartDate);
+            this.Name = "OccupancyReport";
+            this.Text = "OccupancyReport";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label lblTotalBooking;
+        private System.Windows.Forms.TextBox txtAccount;
+        private System.Windows.Forms.Label lblEndDate;
+        private System.Windows.Forms.Label lblStartDate;
+        private System.Windows.Forms.TextBox txtStartDate;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnHome;
+    }
+}
